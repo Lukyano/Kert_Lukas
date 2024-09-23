bb79d9a90345e6a39007d221a455e99be71165b7
Lukyano <lukas.kert@email.cz>
2024-09-23 08:27:44 +0200

    hello world

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Lukyano"
+git config --global user.email "lukas.kert@email.cz"
+echo "přihlášení uživatele proběhlo úspěšně"
+#cd eshop_pro
+#https://fonts.bunny.net
+#https://fonts.google.com

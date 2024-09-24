86414a8d950cf47d6830f2e00da018493376fdfc
Lukyano <lukas.kert@email.cz>
2024-09-24 07:45:29 +0200

    breeze

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,12 @@
+#composer create-project laravel/laravel D
+git config --list
 git config --global user.name "Lukyano"
 git config --global user.email "lukas.kert@email.cz"
 echo "přihlášení uživatele proběhlo úspěšně"
+ls
+dir
 #cd eshop_pro
+#php artisan serve
 #https://fonts.bunny.net
 #https://fonts.google.com
+
